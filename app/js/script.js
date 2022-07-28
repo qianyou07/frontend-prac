4019383ed47b006597db2d403bbd8b01d27aaf38
Qianyou <qianyou@qianyoudeMacBook-Pro.local>
2022-07-28 22:36:32 +0800

    header animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,24 @@
-console.log("hello")
+const overlay = document.querySelector(".overlay");
+const hamburger = document.querySelector(".header_menu");
+const sideMenu = document.querySelector(".side-menu");
+const menuImg = document.querySelector("#menu-img");
+
+
+hamburger.addEventListener('click', function(){
+	if(hamburger.classList.contains('open')){ // Close Hamburger Menu
+		hamburger.classList.remove('open');    
+		overlay.classList.remove('darken');
+		overlay.classList.add('lighten');
+		sideMenu.classList.remove('slide-in');
+		sideMenu.classList.add('slide-out');
+		menuImg.src = "images/icon-menu.svg";
+	  }
+	  else { // Open Hamburger Menu
+		hamburger.classList.add('open');
+		overlay.classList.add('darken');
+		overlay.classList.remove('lighten');
+		sideMenu.classList.add('slide-in');
+		sideMenu.classList.remove('slide-out');
+		menuImg.src = "images/icon-close-menu.svg";
+	  }  
+});
